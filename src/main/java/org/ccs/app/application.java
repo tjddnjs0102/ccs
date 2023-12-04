7b35cd067a8762d3ea02fcd22857f87d9d6e65ea
tjddnjs0102 <rjf654@gmail.com>
2023-12-04 16:06:03 +0900

    test

---
+++ src/main/java/org/ccs/app/application.java
@@ -0,0 +1,13 @@
+package org.ccs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/ccs/app/core/Points.java
@@ -0,0 +1,2 @@
+package org.ccs.app.core;public class Points {
+}
