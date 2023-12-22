f4119a0a9ef3e3eeab0adbdf998e07058a0e0a63
ParkSW <125908866+tjddnjs0102@users.noreply.github.com>
2023-12-23 00:25:59 +0900

    [ feature ] User클래스 '@Column'으로 컬럼명 명시 (#8)
    * JPA ddl-none으로 수정
    
    * ccs_user 테이블 설계/User클래스 Column name 추가

--- src/main/java/org/ccs/app/core/user/domain/User.java
+++ src/main/java/org/ccs/app/core/user/domain/User.java
@@ -1,10 +1,7 @@
 package org.ccs.app.core.user.domain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -24,22 +21,51 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 public class User {
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "type")
     private UserType type;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "parish")
     private Integer parish; // 교구
+
+    @Column(name = "cell_group")
     private Integer cellGroup; //구역
+
+    @Column(name = "fellowship_group")
     private FellowShipGroup fellowShipGroup; // 회별
+
+    @Column(name = "gender")
     private Gender gender;
+
+    @Column(name = "zipcode")
     private String zipcode; // 우편
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(name = "profile_image")
     private String profileImage;
 
+
+    @Column(name = "state")
     private Boolean state;
+
+    @Column(name = "reason")
     private String reason;
 
     // fixme : Audit으로 분리할 것
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
+
