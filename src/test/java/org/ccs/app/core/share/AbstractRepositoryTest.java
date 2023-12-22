23444eadc047b617e412b145d3cfabf0eee77a97
kkihwan22 <107658596+kkihwan22@users.noreply.github.com>
2023-12-22 14:14:37 +0900

    [ feature ] DataJpaTest 준비 (#5)
    - 테스트 전용 설정파일 생성
    - DataJpaTest 공통 추상클래스 생성

---
+++ src/test/java/org/ccs/app/core/share/AbstractRepositoryTest.java
@@ -0,0 +1,14 @@
+package org.ccs.app.core.share;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public abstract class AbstractRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+}
