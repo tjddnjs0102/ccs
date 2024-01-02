343a20554abab7beb9c334ed6eeecc9541f06ca7
kkihwan22 <jinkh97@gmail.com>
2024-01-02 18:00:16 +0900

    feat: 서버사이드 웹페이지 기능추가

--- src/main/java/org/ccs/app/entrypoints/welcome/controller/WelcomeController.java
+++ src/main/java/org/ccs/app/entrypoints/welcome/controller/WelcomeController.java
@@ -1,9 +1,14 @@
 package org.ccs.app.entrypoints.welcome.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
 
+    @RequestMapping("/dashboard")
+    public String welcome() {
 
+        return "index";
+    }
 }
