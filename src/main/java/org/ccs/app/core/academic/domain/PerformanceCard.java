5e32c51eedd78e596c2fa49446c1b128cfdff802
tjddnjs0102 <rjf654@gmail.com>
2024-01-02 19:53:11 +0900

    feat: PerformanceCard 테이블 설계 - Student 테이블 설계 필요

--- src/main/java/org/ccs/app/core/academic/domain/PerformanceCard.java
+++ src/main/java/org/ccs/app/core/academic/domain/PerformanceCard.java
@@ -1,19 +1,42 @@
 package org.ccs.app.core.academic.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.ccs.app.core.share.domain.BaseCreatedAndUpdatedDateTime;
 import org.ccs.app.core.studunt.domain.Student;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.time.LocalDateTime;
 
-public class PerformanceCard {
+@Entity
+@Table(name = "performance_card")
+@DynamicInsert @DynamicUpdate
+@NoArgsConstructor @AllArgsConstructor
+@Getter @ToString
+public class PerformanceCard extends BaseCreatedAndUpdatedDateTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // private Long academicScheduleId; // trade off
+
+//     private Long academicScheduleId; // trade off
+
+    @Column(name = "date")
     private String date; // yyyyMMdd
+
+    @ManyToOne
+    @JoinColumn(name = "ccs_student_id") // 아직 student 테이블 없음
     private Student student;
-    // TODO: study..
-//    @AttributeOverrides({
-//            @AttributeOverride(name = "company", column = @Column(name = "sns_company")),
-//            @AttributeOverride(name = "address", column = @Column(name = "sns_address")),
-//    })
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "attendance", column = @Column(name = "attendance")),
+            @AttributeOverride(name = "homework", column = @Column(name = "homework")),
+            @AttributeOverride(name = "weeklyVerse", column = @Column(name = "weekly_verse")),
+            @AttributeOverride(name = "monthlyVerse", column = @Column(name = "monthly_verse"))
+    })
     private PerformancePoints points;
-    private LocalDateTime createdAt;
 }
--- src/main/java/org/ccs/app/core/academic/domain/PerformancePoints.java
+++ src/main/java/org/ccs/app/core/academic/domain/PerformancePoints.java
@@ -1,8 +1,13 @@
 package org.ccs.app.core.academic.domain;
 
+import jakarta.persistence.Embeddable;
+
 /**
  * Entity가 아니라 VO입니다.
+ * @@Embeddable: 해당 클래스가 다른 엔티티에 포함될 수 있음
  */
+
+@Embeddable
 public class PerformancePoints {
     private Integer attendance;
     private Integer homework;
