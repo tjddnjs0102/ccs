fbd2c7ca332daef16b7cebce9d3011462ccb3288
kkihwan22 <jinkh97@gmail.com>
2024-01-05 16:00:17 +0900

    feat: 서버사이드 웹페이지 기능추가
    - admin template 적용

--- src/main/java/org/ccs/app/entrypoints/welcome/controller/WelcomeController.java
+++ src/main/java/org/ccs/app/entrypoints/welcome/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 
-    @RequestMapping("/dashboard")
+    @RequestMapping("/")
     public String welcome() {
 
         return "index";
