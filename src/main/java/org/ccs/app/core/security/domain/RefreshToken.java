a1170739a97e98bf50efdf8cc40cfa6ce2f085b7
tjddnjs0102 <rjf654@gmail.com>
2024-01-11 02:06:13 +0900

    feat: RefreshToken 엔티티, 테이블 작성

---
+++ src/main/java/org/ccs/app/core/security/domain/RefreshToken.java
@@ -0,0 +1,26 @@
+package org.ccs.app.core.security.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ccs_refresh_tokens")
+@NoArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "expiry_date")
+    private LocalDateTime expiryDate;
+}
