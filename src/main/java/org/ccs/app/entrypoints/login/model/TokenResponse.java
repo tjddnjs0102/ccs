26ec9c592a9a2c4acd721e7769f45ea70e8308f2
tjddnjs0102 <rjf654@gmail.com>
2024-01-11 18:37:09 +0900

    refactor: TokenResponse / @AllArgsConstructor 추가

--- src/main/java/org/ccs/app/entrypoints/login/model/TokenResponse.java
+++ src/main/java/org/ccs/app/entrypoints/login/model/TokenResponse.java
@@ -1,10 +1,12 @@
 package org.ccs.app.entrypoints.login.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class TokenResponse {
     private String accessToken;
     private String refreshToken;
