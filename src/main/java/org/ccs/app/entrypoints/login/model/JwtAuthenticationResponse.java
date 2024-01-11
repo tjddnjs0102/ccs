7190c9322d77a9c395622f919f2394056229f8a0
tjddnjs0102 <rjf654@gmail.com>
2024-01-11 15:28:30 +0900

    feat: JwtAuthenticationResponse에 리프레시토큰 필드 추가, AllArgsConstructor 주석추가

--- src/main/java/org/ccs/app/entrypoints/login/model/JwtAuthenticationResponse.java
+++ src/main/java/org/ccs/app/entrypoints/login/model/JwtAuthenticationResponse.java
@@ -5,8 +5,10 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class JwtAuthenticationResponse {
     private String accessToken;
+    private String refreshToken;
     private String tokenType = "Bearer";
     private Long expiresIn; // 토큰 만료 시간을 나타내는 필드
 }
