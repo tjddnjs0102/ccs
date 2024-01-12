5cd0b0c81d3f9a4a619a4ac6bff0d8f36f47b706
tjddnjs0102 <rjf654@gmail.com>
2024-01-13 02:49:13 +0900

    refactor: `JwtAuthenticationResponse`클래스에 `accessToken` 생성자 추가 - `LoginController`에서 사용 위함

--- src/main/java/org/ccs/app/entrypoints/login/model/JwtAuthenticationResponse.java
+++ src/main/java/org/ccs/app/entrypoints/login/model/JwtAuthenticationResponse.java
@@ -9,4 +9,8 @@ public class JwtAuthenticationResponse {
     private String accessToken;
     private String tokenType = "Bearer";
     private Long expiresIn; // 토큰 만료 시간을 나타내는 필드
+
+    public JwtAuthenticationResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
 }
