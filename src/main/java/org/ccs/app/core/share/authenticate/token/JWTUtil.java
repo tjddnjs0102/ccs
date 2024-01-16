5b0dfd2720f16967fd5c2870f8d9494e814d63e9
kkihwan22 <107658596+kkihwan22@users.noreply.github.com>
2024-01-16 17:30:18 +0900

    [JWT 기능 개선] (#38)
    * [JWT 기능 개선]
    
    - JWT 토큰의 타입을 관리하는 JWTType.java 추가
    - JWTType에서 토큰의 유효시간을 관리할 수 있도록 JWTProperties.java 추가
    - TokenProvider 클래스 이름 변경 -> JWTUtil.java
    - JWTUtil에 토큰 생성, 검증, 값 추출 하는 메서드 구현
    
    해당 클래스를 활용해서 로그인 시 토큰을 생성하고 전달하는 기능을 마무리해주세요.
    
    * [JWT 기능 개선]
    
    - LoginService 등 conflict 해결
    
    * [JWT 기능 개선]
    
    - LoginService 등 conflict 해결
    - 테스트 코드 추가
    - 의존성 추가
    
    JWT 라이브러리가 spring boot 3 또는 JDK17을 지원하는지 확인이 필요합니다.

---
+++ src/main/java/org/ccs/app/core/share/authenticate/token/JWTUtil.java
@@ -0,0 +1,72 @@
+package org.ccs.app.core.share.authenticate.token;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Getter
+public class JWTUtil {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public String generate(JWTType type, Long userId) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 3600);
+
+        return Jwts.builder()
+                .setSubject(Long.toString(userId)) // 토큰 subject를 사용자 ID로 설정
+                .setIssuedAt(now)
+                .setExpiration(expiryDate) // 만료시간 1시간
+                .signWith(SignatureAlgorithm.HS512, jwtSecret) // 토큰 서명
+                .compact();
+    }
+
+    public boolean verify(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public Object decode(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+
+    // 사용자 ID를 기반으로 JWT 토큰을 생성
+    @Deprecated
+    public String generateToken(Long userId) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 3600);
+
+        // JWT 토큰을 생성
+        return Jwts.builder()
+                .setSubject(Long.toString(userId)) // 토큰 subject를 사용자 ID로 설정
+                .setIssuedAt(now)
+                .setExpiration(expiryDate) // 만료시간 1시간
+                .signWith(SignatureAlgorithm.HS512, jwtSecret) // 토큰 서명
+                .compact(); // JWT 토큰을 문자열로 압축하여 반환
+    }
+
+    @Deprecated
+    public String generateRefreshToken(Long userId) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 640000);
+
+        return Jwts.builder()
+                .setSubject(Long.toString(userId))
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+}
--- src/main/java/org/ccs/app/entrypoints/login/service/LoginService.java
+++ src/main/java/org/ccs/app/entrypoints/login/service/LoginService.java
@@ -1,42 +1,45 @@
 package org.ccs.app.entrypoints.login.service;
 
 import lombok.RequiredArgsConstructor;
-import org.ccs.app.core.security.JwtTokenProvider;
-import org.ccs.app.core.share.exception.auth.NoSuchUserException;
+import org.ccs.app.core.share.authenticate.token.JWTType;
+import org.ccs.app.core.share.authenticate.token.JWTUtil;
 import org.ccs.app.core.user.application.usecase.LoginUsecase;
 import org.ccs.app.core.user.domain.UserAccount;
-import org.ccs.app.core.user.infra.repository.UserAccountRepository;
 import org.ccs.app.entrypoints.login.model.JwtAuthenticationResponse;
 import org.ccs.app.entrypoints.login.model.LoginRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+// TODO: 혹시나 불필요한 부분이 있다면 제거해주세요. 개선할 수 있는 내용이 있다면 변경해도 좋습니다.!!
+
 @RequiredArgsConstructor
 @Service
 public class LoginService {
     private final Logger log = LoggerFactory.getLogger(LoginService.class);
+
     private final LoginUsecase loginUsecase;
-    private final UserAccountRepository userAccountRepository;
-    private final JwtTokenProvider tokenProvider;
+    private final JWTUtil jwtUtil;
 
     public String authenticate(LoginRequest loginRequest) {
         UserAccount account = loginUsecase.login(loginRequest.getEmail(), loginRequest.getPassword());
         log.debug("[authenticated] account: {}", account);
-        return tokenProvider.generateToken(account.getId());
+
+        return jwtUtil.generate(JWTType.ACCESS, account.getId());
     }
 
-    // 리프레시 토큰 생성 메서드
+    // TODO: JWTUtil을 활용해서 코드를 수정하세요.
     public String createRefreshToken(LoginRequest loginRequest) {
         UserAccount account = loginUsecase.login(loginRequest.getEmail(), loginRequest.getPassword());
         log.debug("[refresh token created] account: {}", account);
-        return tokenProvider.generateRefreshToken(account.getId());
+        return jwtUtil.generateRefreshToken(account.getId());
     }
 
+    // TODO: JWTUtil을 활용해서 코드를 수정하세요.
     public JwtAuthenticationResponse authenticateAndCreateTokens(LoginRequest loginRequest) {
         UserAccount account = loginUsecase.login(loginRequest.getEmail(), loginRequest.getPassword());
-        String jwt = tokenProvider.generateToken(account.getId());
-        String refreshToken = tokenProvider.generateRefreshToken(account.getId());
+        String jwt = jwtUtil.generateToken(account.getId());
+        String refreshToken = jwtUtil.generateRefreshToken(account.getId());
 
         return new JwtAuthenticationResponse(jwt, refreshToken);
     }
