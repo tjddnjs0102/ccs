361b438f6f5302fdcc72d9f6613bd650ba1f78b9
ParkSW <125908866+tjddnjs0102@users.noreply.github.com>
2024-01-17 03:10:19 +0900

    refactor:열거형 Role 클래스에 다국어 적용 (#39)
    - 각 직급별 한글 명칭 재정의 필요

--- src/main/java/org/ccs/app/core/user/domain/Role.java
+++ src/main/java/org/ccs/app/core/user/domain/Role.java
@@ -1,6 +1,17 @@
 package org.ccs.app.core.user.domain;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public enum Role {
-    // TODO: display name / 다국어 적용
-    ADMIN, STAFF, CHIEF, OFFICER, MANAGER, TEACHER,
+    ADMIN, STAFF, CHIEF, OFFICER, MANAGER, TEACHER;
+
+    public String getDisplayName(Locale locale) {
+        return Role.getDisplayNameForRole(this, locale);
+    }
+
+    private static String getDisplayNameForRole(Role role, Locale locale) {
+        ResourceBundle messages = ResourceBundle.getBundle("RoleMessages", locale);
+        return messages.getString(role.name());
+    }
 }
