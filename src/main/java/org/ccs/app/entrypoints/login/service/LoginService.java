ad714a9fae40f12295a544c3da1ea7dc1736a366
tjddnjs0102 <rjf654@gmail.com>
2024-01-16 13:48:29 +0900

    refactor: createRefreshToken 메서드 코드 수정 - 외부에서 다른 pc로 작업하기 위해 push 합니다.

--- src/main/java/org/ccs/app/entrypoints/login/service/LoginService.java
+++ src/main/java/org/ccs/app/entrypoints/login/service/LoginService.java
@@ -12,7 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor // final 필드에 대한 생성자 자동으로 생성
+@RequiredArgsConstructor
 @Service
 public class LoginService {
     private final Logger log = LoggerFactory.getLogger(LoginService.class);
@@ -28,16 +28,9 @@ public class LoginService {
 
     // 리프레시 토큰 생성 메서드
     public String createRefreshToken(LoginRequest loginRequest) {
-        // 이메일을 기반으로 사용자 계정 조회
-        UserAccount userAccount = getUserByEmail(loginRequest.getEmail());
-
-        // 사용자 계정이 있는 경우 리프레시 토큰 생성 및 반환
-        if (userAccount != null) {
-            return tokenProvider.generateRefreshToken(userAccount.getId());
-        } else {
-            // 계정이 없는 경우 예외 처리
-            throw new IllegalArgumentException("User not found with email: " + loginRequest.getEmail());
-        }
+        UserAccount account = loginUsecase.login(loginRequest.getEmail(), loginRequest.getPassword());
+        log.debug("[refresh token created] account: {}", account);
+        return tokenProvider.generateRefreshToken(account.getId());
     }
 
     public JwtAuthenticationResponse authenticateAndCreateTokens(LoginRequest loginRequest) {
@@ -53,7 +46,6 @@ public class LoginService {
         }
     }
 
-    // README에 get,find의 차이점을 적어두었습니다.
     private UserAccount getUserByEmail(String email) {
         return userAccountRepository.findByEmail(email)
                 .orElseThrow(NoSuchUserException::new);
