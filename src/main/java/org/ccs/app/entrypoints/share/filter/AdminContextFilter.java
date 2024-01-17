f93e4f042e3322923fb7959582030fd1c7b469ad
tjddnjs0102 <rjf654@gmail.com>
2024-01-17 17:45:04 +0900

    feat: ADD ADMIN 권한 확인 로직 IN AdminContextFilter.java

--- src/main/java/org/ccs/app/entrypoints/share/filter/AdminContextFilter.java
+++ src/main/java/org/ccs/app/entrypoints/share/filter/AdminContextFilter.java
@@ -2,6 +2,10 @@ package org.ccs.app.entrypoints.share.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.ccs.app.core.share.authenticate.Authenticate;
+import org.ccs.app.core.share.authenticate.AuthenticateHolder;
+import org.ccs.app.core.user.domain.Role;
 import org.springframework.core.annotation.Order;
 
 import java.io.IOException;
@@ -10,11 +14,16 @@ import java.io.IOException;
 @Order(31)
 public class AdminContextFilter implements Filter {
 
-    // TODO: 로직을 작성하세요.
-    // 힌트 : threadlocal 에 담겨있는 authenticate 객체를 활용하면 됩니다. 거기서 role 정보를 꺼내서 확인하세요.
+    Authenticate authenticate = AuthenticateHolder.getLoginUser();
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(request, response);
+        if (authenticate != null && authenticate.getRoles().contains(Role.ADMIN)) {
+            chain.doFilter(request, response);
+        }else { // ADMIN 권한이 없는 경우
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403
+            httpResponse.getWriter().write("Access Denied: Insufficient Permissions");
+        }
     }
 }
