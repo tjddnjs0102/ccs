f59573b72ef4a48850d2388e35601f74f2d7e2bf
tjddnjs0102 <rjf654@gmail.com>
2024-01-20 02:59:37 +0900

    refactor: ErrorCode 다국어 적용 Version-1

--- src/main/java/org/ccs/app/core/share/exception/ErrorCode.java
+++ src/main/java/org/ccs/app/core/share/exception/ErrorCode.java
@@ -11,23 +11,28 @@ import lombok.Getter;
  */
 public enum ErrorCode {
     // TODO : message 에 다국어 적용
-    UNKNOWN(100, ""),
+    UNKNOWN(100, "", ""),
 
-    UNAUTHORIZED_ACCESS(101, "사용자 인증에 실패하였습니다."),
-    NO_SUCH_USER(102, "로그인 아이디를 찾을 수 없습니다."),
-    INCORRECT_PASSWORD(102, "로그인 패스워드가 일치하지 않습니다."),
+    UNAUTHORIZED_ACCESS(101, "사용자 인증에 실패하였습니다.", "Authentication failure."),
+    NO_SUCH_USER(102, "로그인 아이디를 찾을 수 없습니다.", "Login ID not found."),
+    INCORRECT_PASSWORD(103, "로그인 패스워드가 일치하지 않습니다.", "Incorrect login password."),
 
-    NOT_FOUND_MENU(1000, ""),
+    NOT_FOUND_MENU(1000, "", ""),
     ;
 
     @Getter
     int code;
 
     @Getter
-    String message;
+    String koMessage;
 
-    ErrorCode(int code, String message) {
+    @Getter
+    String enMessage;
+
+
+    ErrorCode(int code, String koMessage, String enMessage) {
         this.code = code;
-        this.message = message;
+        this.koMessage = koMessage;
+        this.enMessage = enMessage;
     }
 }
