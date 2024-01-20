5bb7fb58da5262c73bc394796a55d113491c1b4a
ParkSW <125908866+tjddnjs0102@users.noreply.github.com>
2024-01-20 15:46:58 +0900

    JWTUtil를 사용한 LoginService 리팩토링 (#43)
    * [JWT 기능 개선]
    
    - JWT 토큰의 타입을 관리하는 JWTType.java 추가
    - JWTType에서 토큰의 유효시간을 관리할 수 있도록 JWTProperties.java 추가
    - TokenProvider 클래스 이름 변경 -> JWTUtil.java
    - JWTUtil에 토큰 생성, 검증, 값 추출 하는 메서드 구현
    
    해당 클래스를 활용해서 로그인 시 토큰을 생성하고 전달하는 기능을 마무리해주세요.
    
    * [JWT 기능 개선]
    
    - LoginService 등 conflict 해결
    
    * [JWT 기능 개선]
    
    - LoginService 등 conflict 해결
    - 테스트 코드 추가
    - 의존성 추가
    
    JWT 라이브러리가 spring boot 3 또는 JDK17을 지원하는지 확인이 필요합니다.
    
    * refactor: Deprecated된 메서드 사용 중인 부분 수정 (`generate()` 사용)
    
    * refactor: Deprecated된 메서드 제거 (generateToken(), generateRefreshToken()) IN JWTUtil.java
    
    * refactor: 코드 중복 제거, authenticate(), createRefreshToken() 사용
    
    * refactor: authenticateAndCreateTokens메서드 rename IN LoginService.java
    - 유저가 인증되지 않았을 때 예외 발생
    
    * refactor: 불필요한 예외처리 제거 IN LoginService.java
    
    ---------
    
    Signed-off-by: ParkSW <125908866+tjddnjs0102@users.noreply.github.com>
    Co-authored-by: kkihwan22 <jinkh97@gmail.com>

--- src/main/java/org/ccs/app/core/share/authenticate/token/JWTUtil.java
+++ src/main/java/org/ccs/app/core/share/authenticate/token/JWTUtil.java
@@ -40,33 +40,4 @@ public class JWTUtil {
     public Object decode(String token) {
         return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
     }
-
-
-    // 사용자 ID를 기반으로 JWT 토큰을 생성
-    @Deprecated
-    public String generateToken(Long userId) {
-        Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + 3600);
-
-        // JWT 토큰을 생성
-        return Jwts.builder()
-                .setSubject(Long.toString(userId)) // 토큰 subject를 사용자 ID로 설정
-                .setIssuedAt(now)
-                .setExpiration(expiryDate) // 만료시간 1시간
-                .signWith(SignatureAlgorithm.HS512, jwtSecret) // 토큰 서명
-                .compact(); // JWT 토큰을 문자열로 압축하여 반환
-    }
-
-    @Deprecated
-    public String generateRefreshToken(Long userId) {
-        Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + 640000);
-
-        return Jwts.builder()
-                .setSubject(Long.toString(userId))
-                .setIssuedAt(now)
-                .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-                .compact();
-    }
 }
