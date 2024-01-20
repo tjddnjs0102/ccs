415c8ccbbac6ee0dd5340753147c90df90ac5c86
tjddnjs0102 <rjf654@gmail.com>
2024-01-20 15:43:59 +0900

    feat: ApiContextFilter 로직 구현

--- src/main/java/org/ccs/app/entrypoints/share/filter/ApiContextFilter.java
+++ src/main/java/org/ccs/app/entrypoints/share/filter/ApiContextFilter.java
@@ -2,6 +2,10 @@ package org.ccs.app.entrypoints.share.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.ccs.app.core.share.authenticate.Authenticate;
+import org.ccs.app.core.share.authenticate.AuthenticateHolder;
+import org.ccs.app.entrypoints.share.model.ContentBody;
 import org.springframework.core.annotation.Order;
 
 import java.io.IOException;
@@ -10,11 +14,28 @@ import java.io.IOException;
 @Order(32)
 public class ApiContextFilter implements Filter {
 
-    // TODO: 로직을 작성하세요.
-    // 힌트 : threadlocal 에 담겨있는 authenticated 를 확인해서 처리할 수 있습니다.
-
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        Authenticate authenticate = AuthenticateHolder.get();
+
+        if (authenticate == null) {
+            sendUnauthenticatedResponse(response);
+            return;
+        }
+
         chain.doFilter(request, response);
+    }
+
+    private void sendUnauthenticatedResponse(ServletResponse response) throws IOException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        ContentBody<String> unauthenticatedBody = new ContentBody<>(
+                HttpServletResponse.SC_UNAUTHORIZED, // 401
+                "Unauthorized: 로그인이 필요합니다.", // authenticate 객체가 없다는 것은 로그인을 하지 않은 상태라고 생각했습니다.
+                "",
+                null);
+
+        String serializedBody = ContentBody.serialize(unauthenticatedBody);
+        httpResponse.setContentType("application/json");
+        httpResponse.getWriter().write(serializedBody);
     }
 }
