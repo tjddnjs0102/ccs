79aa900163105e1bb74e9cd46f362f601c4dbbb0
tjddnjs0102 <rjf654@gmail.com>
2024-01-22 01:43:41 +0900

    refactor: serialize 메서드 제거 하고 ObjectMapper 사용

--- src/main/java/org/ccs/app/entrypoints/share/filter/ApiContextFilter.java
+++ src/main/java/org/ccs/app/entrypoints/share/filter/ApiContextFilter.java
@@ -1,5 +1,6 @@
 package org.ccs.app.entrypoints.share.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,12 +8,15 @@ import org.ccs.app.core.share.authenticate.Authenticate;
 import org.ccs.app.core.share.authenticate.AuthenticateHolder;
 import org.ccs.app.entrypoints.share.model.ContentBody;
 import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@WebFilter("/api/*")
-@Order(32)
+@Component
 public class ApiContextFilter implements Filter {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -29,13 +33,13 @@ public class ApiContextFilter implements Filter {
     private void sendUnauthenticatedResponse(ServletResponse response) throws IOException {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         ContentBody<String> unauthenticatedBody = new ContentBody<>(
-                HttpServletResponse.SC_UNAUTHORIZED, // 401
-                "Unauthorized: 로그인이 필요합니다.", // authenticate 객체가 없다는 것은 로그인을 하지 않은 상태라고 생각했습니다.
+                HttpServletResponse.SC_UNAUTHORIZED,
+                "Unauthorized: 로그인이 필요합니다.",
                 "",
                 null);
 
-        String serializedBody = ContentBody.serialize(unauthenticatedBody);
+
         httpResponse.setContentType("application/json");
-        httpResponse.getWriter().write(serializedBody);
+        httpResponse.getWriter().write(mapper.writeValueAsString(unauthenticatedBody));
     }
 }
