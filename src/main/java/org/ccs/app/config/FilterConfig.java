6ca828a98168e2ed042ec4352ec469848c3d432f
tjddnjs0102 <rjf654@gmail.com>
2024-01-24 09:37:08 +0900

    FilterConfig에 누락된 merge 내용 적용

--- src/main/java/org/ccs/app/config/FilterConfig.java
+++ src/main/java/org/ccs/app/config/FilterConfig.java
@@ -1,6 +1,9 @@
 package org.ccs.app.config;
 
+import org.ccs.app.entrypoints.share.filter.AdminContextFilter;
 import org.ccs.app.entrypoints.share.filter.ApiContextFilter;
+import org.ccs.app.entrypoints.share.filter.JWTFilter;
+import org.ccs.app.entrypoints.share.filter.MDCFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +12,48 @@ import org.springframework.context.annotation.Configuration;
 public class FilterConfig {
 
     @Bean
-    public FilterRegistrationBean<ApiContextFilter> apiContextFilterRegistration() {
-        FilterRegistrationBean<ApiContextFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new ApiContextFilter());
-        registration.addUrlPatterns("/api/*");
-        registration.setOrder(32);
-        return registration;
+    public FilterRegistrationBean<MDCFilter> mdcFilter() {
+        FilterRegistrationBean<MDCFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new MDCFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(Integer.MIN_VALUE);
+        return registrationBean;
+    }
+
+    // TODO: logging을 위해 request의 스트림을 소진해서 실제 requestbody가 null이 되는 현상 수정
+//    @Bean
+//    public FilterRegistrationBean<LoggingFilter> loggingFilter() {
+//        FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new LoggingFilter());
+//        registrationBean.addUrlPatterns("/*");
+//        registrationBean.setOrder(10);
+//        return registrationBean;
+//    }
+
+    @Bean
+    public FilterRegistrationBean<JWTFilter> jwtFilter() {
+        FilterRegistrationBean<JWTFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JWTFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(20);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<ApiContextFilter> apiContextFilter() {
+        FilterRegistrationBean<ApiContextFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new ApiContextFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(21);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<AdminContextFilter> adminContextFilter() {
+        FilterRegistrationBean<AdminContextFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new AdminContextFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(22);
+        return registrationBean;
     }
 }
