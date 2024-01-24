ee83110a4623cb0e6136360b2d5392ac3e5d2039
tjddnjs0102 <rjf654@gmail.com>
2024-01-24 10:08:57 +0900

    ApiContextFilter 스프링 빈 중복 제거

--- src/main/java/org/ccs/app/entrypoints/share/filter/ApiContextFilter.java
+++ src/main/java/org/ccs/app/entrypoints/share/filter/ApiContextFilter.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
 public class ApiContextFilter implements Filter {
 
     private static final ObjectMapper mapper = new ObjectMapper();
