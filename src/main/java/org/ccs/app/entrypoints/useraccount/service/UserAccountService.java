120b9bf5b116cf24aee92707700bac4075f40ff8
tjddnjs0102 <rjf654@gmail.com>
2024-02-01 12:36:02 +0900

    refactor: 서비스 클래스를 인터페이스로 변경

--- src/main/java/org/ccs/app/entrypoints/useraccount/service/UserAccountService.java
+++ src/main/java/org/ccs/app/entrypoints/useraccount/service/UserAccountService.java
@@ -1,17 +1,9 @@
 package org.ccs.app.entrypoints.useraccount.service;
 
-import lombok.AllArgsConstructor;
+
 import org.ccs.app.entrypoints.useraccount.model.UserAccountRequest;
-import org.springframework.stereotype.Service;
 
-
-@Service
-@AllArgsConstructor
-public class UserAccountService {
-
-    public void signup(UserAccountRequest userAccountRequest) {
-        // 회원가입 로직 구현 필요
-        // 여기에 userAccountRequest를 사용하여 실제 회원가입 처리 로직을 구현
-    }
+public interface UserAccountService {
+    Long signup(UserAccountRequest userAccountRequest);
 }
 
