9a5ef0ff5312224ff88df8d63d9c681c5eabc0df
tjddnjs0102 <rjf654@gmail.com>
2024-02-01 12:38:10 +0900

    feat: 컨트롤러에 signup 메소드 추가 및 로그 설정, 엔드포인트를 member에서 account로 수정

--- src/main/java/org/ccs/app/entrypoints/useraccount/controller/UserAccountController.java
+++ src/main/java/org/ccs/app/entrypoints/useraccount/controller/UserAccountController.java
@@ -5,31 +5,40 @@ import org.ccs.app.entrypoints.useraccount.model.UserAccountRequest;
 import org.ccs.app.entrypoints.useraccount.service.UserAccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 @Controller
 @AllArgsConstructor
 public class UserAccountController {
 
     private UserAccountService userAccountService;
+    private static final Logger logger = LoggerFactory.getLogger(UserAccountController.class);
 
     @GetMapping("/")
     public String index() {
         return "/home/index"; // 홈페이지 경로 임의로 적어두었습니다. 확인 후 수정 필요
     }
 
-    @GetMapping("/public/member/signup")
+    @GetMapping("/public/account/signup")
     public String signupForm(Model model) {
         model.addAttribute("userAccount", new UserAccountRequest());
-
-        return "/public/member/signupForm";
+        return "/public/account/signupForm";
     }
 
-    @PostMapping("/public/member/signup")
-    public String signup(UserAccountRequest userAccountRequest) {
-//        userAccountService.signUp(userAccountRequest); // 회원가입 로직 수행 (아직 서비스 미구현)
+    @PostMapping("/public/account/signup")
+    public String signup(UserAccountRequest userAccountRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/public/account/signupForm";
+        }
 
+        userAccountService.signup(userAccountRequest);
+        logger.info("회원가입 성공: {}", userAccountRequest.getUsername());
         return "redirect:/";
     }
 }
