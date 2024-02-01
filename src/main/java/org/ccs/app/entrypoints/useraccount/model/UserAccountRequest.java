e61975b7e8fdc601d2e4dbea349f721fc247e22f
tjddnjs0102 <rjf654@gmail.com>
2024-02-01 12:35:34 +0900

    feat: UserAccountRequest 항목 추가

--- src/main/java/org/ccs/app/entrypoints/useraccount/model/UserAccountRequest.java
+++ src/main/java/org/ccs/app/entrypoints/useraccount/model/UserAccountRequest.java
@@ -1,9 +1,12 @@
 package org.ccs.app.entrypoints.useraccount.model;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.ccs.app.core.user.domain.Gender;
 
 @Getter @Setter
 public class UserAccountRequest {
@@ -17,4 +20,18 @@ public class UserAccountRequest {
 
     @NotBlank(message = "request.valid.password.required")
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private String phoneNumber;
+
+    private Integer parish;
+
+    private Integer cellGroup;
+
+
+    private String zipcode;
+
+    private String address;
 }
